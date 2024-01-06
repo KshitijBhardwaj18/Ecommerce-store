09c3fceaf1cf09e9a20399a11eab9418f54d7538
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2024-01-06 15:26:11 +0530

    Assembled routes and made a dummy footer

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return ( 
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-x text-black">
+                    &copy; 2023 BhardwajStore, Inc. All rights reserved
+                </p>
+            </div>
+
+        </footer>
+     );
+}
+
+export default Footer;
