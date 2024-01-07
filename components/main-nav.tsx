a23cb5caa84ee080cb80fc8923937aa64b6dde77
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2024-01-07 23:50:14 +0530

    Added Jay shree ram

---
+++ components/main-nav.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+interface MainNavProps {
+  data: any;
+}
+
+const MainNav: React.FC<MainNavProps> = ({ data }) => {
+  const pathname = usePathname();
+
+  const routes = data.map((route) => ({
+    href: `/category/${route.id}`,
+    label: route.name,
+    active: pathname === `/category/${route.id}`,
+  }));
+
+  return <nav>main-nav</nav>;
+};
+
+export default MainNav;
+
+//जय श्री राम जय श्री राम जय श्री राम जय श्री राम जय श्री राम जय श्री राम जय श्री राम जय श्री राम जय श्री राम जय श्री राम
