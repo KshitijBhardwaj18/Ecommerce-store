07e2a13e2ff0488a5a379b4378d65600bd7d3f98
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2024-01-08 23:52:22 +0530

    Completed billboard section

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,23 @@
-const HomePage = () => {
+import Container from "@/components/ui/container"
+import Billboard from "@/components/billboard";
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+
+export const revalidate = 0;
+
+
+const HomePage  = async () => {
+    const products = await getProducts({isFeatured: true})
+    const billboard = await getBillboard("f8ec2aeb-c854-456b-b5d2-fb9a2001f008");
+
     return (
-        <div>Hello Store</div>
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard}/>
+            </div>
+
+        
+        </Container>
     )
 }
 
