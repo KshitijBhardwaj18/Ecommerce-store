2ef6e43ed6d92e7a38aec1401315859e96768183
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2024-01-09 14:42:52 +0530

    Progressed on individual product page and completed product card

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,24 +1,26 @@
-import Container from "@/components/ui/container"
+import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
 import getBillboard from "@/actions/get-billboard";
 import getProducts from "@/actions/get-products";
+import ProductList from "@/components/products-list";
 
 export const revalidate = 0;
 
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("f8ec2aeb-c854-456b-b5d2-fb9a2001f008");
 
-const HomePage  = async () => {
-    const products = await getProducts({isFeatured: true})
-    const billboard = await getBillboard("f8ec2aeb-c854-456b-b5d2-fb9a2001f008");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
 
-    return (
-        <Container>
-            <div className="space-y-10 pb-10">
-                <Billboard data={billboard}/>
-            </div>
-
-        
-        </Container>
-    )
-}
+        <div className="flex flex-col gap-y-8 px-3 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
 
 export default HomePage;
